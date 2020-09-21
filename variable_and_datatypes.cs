844bc260f6fcb2c5c87f55397014414214f1218c
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-09-21 12:51:43 +0530

    Data type: Double

--- variable_and_datatypes.cs
+++ variable_and_datatypes.cs
@@ -12,6 +12,18 @@ namespace C_Sharp
             int num2 = 7;
             int sum = num1 + num2;
             Console.WriteLine("The sum of " + num1 + " and " + num2 + " is " + sum);
+
+            double d1 = 3.5;
+            double d2 = 3.227;
+            double d3 = d1 + d2;
+            Console.WriteLine("The sum of " + d1 + " and " + d2 + " is " + d3);
+            Console.WriteLine("The sum of " + num1 + " and " + d1 + " is " + (num1 + d1));
+            Console.WriteLine(d1 + " divided by " + d2 + " is " + (d1 / d2));
+            Console.WriteLine(d1 + " divided by " + num1 + " is " + (d1 / num1));
+
+            //you can not store the divided into int 
+            //int x = d1 / num1;
+            // for this you need type conversion 
         }            
     }
 }
