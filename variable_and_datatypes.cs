fde74d6b8e8f95e712c077fb6455f423f86bc0df
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-09-21 12:02:22 +0530

    Make variable to method variable, so to use it we do not have make it static

--- variable_and_datatypes.cs
+++ variable_and_datatypes.cs
@@ -6,13 +6,13 @@ namespace C_Sharp
 {
     class Variable_Class
     {
-        static int age = 15;
-        // This is variable type of Integer and name is age
-        static int age1;
-
         public static void printData()
         {
-            Console.WriteLine(age1);
+
+            int age = 15;
+            // This is variable type of Integer and name is age
+
+            Console.WriteLine(age);
             //  Print a variable age
         }
             
