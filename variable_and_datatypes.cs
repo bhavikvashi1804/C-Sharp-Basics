ca6b0bfc860511eba7a531ba668267df6d120684
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-09-21 11:58:44 +0530

    Default Value Assign to int variable = 0

--- variable_and_datatypes.cs
+++ variable_and_datatypes.cs
@@ -8,10 +8,11 @@ namespace C_Sharp
     {
         static int age = 15;
         // This is variable type of Integer and name is age
+        static int age1;
 
         public static void printData()
         {
-            Console.WriteLine(age);
+            Console.WriteLine(age1);
             //  Print a variable age
         }
             
