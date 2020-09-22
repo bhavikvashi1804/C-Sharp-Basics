26e74e67af91b540669ea319bd89ecf3df51c1f6
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-09-22 19:01:31 +0530

    Tic Toc Toe Game

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace C_Sharp
         static void Main(string[] args)
         {
 
-            ArraysClass.MultiDimensionalArray();
+            TicTacToe.PlayTicTacToe();
             Console.Read();
 
         }
