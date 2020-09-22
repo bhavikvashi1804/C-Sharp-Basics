df4123852ab9a911d60c57c6ffd44d50b7211178
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-09-22 13:36:48 +0530

    Create class and make two object of that class

---
+++ Human.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp
+{
+    //class is blueprint of datatype 
+    class Human
+    {
+        //member variable
+        string firstName, lastName;
+        //to access member variable you have to make it public
+        //if you do not make it public then you can only use it in this class
+        public string fullName="Human Name";
+        //provide default value: fullName to Human name 
+
+
+        //methods
+        public void IntroduceMySelf()
+        {
+            Console.WriteLine("Hi, I am {0}", fullName);
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace C_Sharp
     {
         static void Main(string[] args)
         {
-            LoopClass.LoopTask();
+            //create a object of Human Class
+            Human h1 = new Human();
+            //access the public variable
+            h1.fullName = "Bhavik Vashi";
+            //call the class method
+            h1.IntroduceMySelf();
+
+            Human h2 = new Human();
+            h2.fullName = "Raj Patel";
+            h2.IntroduceMySelf();
+
             Console.ReadKey();
         }
     }
