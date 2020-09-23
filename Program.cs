5e096e70ea376ee413b10cd1dbd9d5bfbd2e9ed3
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-09-23 11:32:08 +0530

    Timer concept

--- Program.cs
+++ Program.cs
@@ -9,6 +9,14 @@ namespace C_Sharp
 
             VideoPost videoPost1 = new VideoPost("Happy Birthday","Meet Patel","https://sdsad.com",true,120);
             Console.WriteLine(videoPost1);
+
+
+            videoPost1.PlayVideo();
+            Console.WriteLine("Press any key to stop video");
+            Console.ReadLine();
+            videoPost1.StopVideo();
+
+
             Console.ReadLine();
         }
     }
--- VideoPost.cs
+++ VideoPost.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace C_Sharp
 {
     class VideoPost:Post
     {
+
+        protected bool isVideoPlaying = false;
+        protected int currentDuration = 0;
+        Timer timer;
+        
         public int VideoLength { get; set; }
         public string VideoURL { get; set; }
 
@@ -27,6 +33,44 @@ namespace C_Sharp
         public override string ToString()
         {
             return string.Format("{0} - {1} - {2} of {3} mins- by {4}", ID, Title, VideoURL, VideoLength,SendByUserName);
+        }
+
+        public void PlayVideo()
+        {
+            if (!isVideoPlaying)
+            {
+                isVideoPlaying = true;
+                Console.WriteLine("Video is playing ...");
+                timer = new Timer(TimerCallBack, null, 0,600);
+            }
+        }
+
+        private void TimerCallBack(object o)
+        {
+            //timer method accepts the timer call back
+            //this call method is used to execute when each time specified period is over
+            //in our case it is 600ms  = 1s 
+            if(currentDuration < this.VideoLength)
+            {
+                currentDuration++;
+                Console.WriteLine("Video is at {0}s", currentDuration);
+                GC.Collect();
+            }
+            else
+            {
+                StopVideo();
+            }
+        }
+
+        public void StopVideo()
+        {
+            if (isVideoPlaying)
+            {
+                isVideoPlaying = false;
+                Console.WriteLine("Video is stopped at {0}s", currentDuration);
+                currentDuration = 0;
+                timer.Dispose();
+            }
         }
 
 
