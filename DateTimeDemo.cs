194de5af55b79b4e205503aba1ba2d683aabfed3
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-09-24 14:37:46 +0530

    DateTime Basics

--- DateTimeDemo.cs
+++ DateTimeDemo.cs
@@ -10,15 +10,39 @@ namespace C_Sharp
         {
             Console.WriteLine("Enter your birthdate in YYYY/MM/DD format");
             string input = Console.ReadLine();
-            DateTime dob = Convert.ToDateTime(input) ;
-            var today = DateTime.Today;
-            var age = today.Year - dob.Year;
-            if (dob.Date > today.AddYears(-age)) age--;
+            DateTime dob= new DateTime();
+            if(DateTime.TryParse(input, out dob))
+            {
+                var today = DateTime.Today;
+                var age = today.Year - dob.Year;
+                if (dob.Date > today.AddYears(-age)) age--;
 
-            Console.WriteLine(age);
+                Console.WriteLine(age);
+
+                TimeSpan daySpend = DateTime.Now.Subtract(dob);
+                Console.WriteLine("Days spend: {0}", daySpend.Days);
+
+            }
+           
 
 
 
+        }
+
+        public static void DateBasics()
+        {
+            DateTime d1 = new DateTime(2019, 12, 04);
+            Console.WriteLine(d1.ToString());
+            Console.WriteLine("Years : "+d1.Year);
+            Console.WriteLine("Month : "+d1.Month);
+            Console.WriteLine("Day : "+d1.Day);
+
+            Console.WriteLine("Today is "+DateTime.Now);
+            Console.WriteLine("It is {0} o'clock {1} minutes and {2} seconds", DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Second);
+            Console.WriteLine("Day of week is " + DateTime.Now.DayOfWeek);
+            Console.WriteLine("Tomorrow is " + DateTime.Now.AddDays(1));
+
+            Console.WriteLine("In Feb 2020 total no of days " + DateTime.DaysInMonth(2020,02));
         }
     }
 }
