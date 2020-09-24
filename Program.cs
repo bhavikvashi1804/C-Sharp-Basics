7b3efb88587ea376c9f72ee8e09809999b0923e7
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-09-24 13:56:39 +0530

    Regex

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace C_Sharp
     {
         static void Main(string[] args)
         {
-            RandomDemo.ExplainRandom();
+            RegularExpressionsDemo.ExplainRegularExpressions();
             Console.Read();
         }
     }
---
+++ RegularExpressionsDemo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace C_Sharp
+{
+    class RegularExpressionsDemo
+    {
+        public static void ExplainRegularExpressions()
+        {
+            string pattern = @"\d";
+            Regex regex1 = new Regex(pattern);
+            string testString = "Hi there my number is 123456789";
+
+            MatchCollection matches = regex1.Matches(testString);
+            Console.WriteLine("Found {0} digits",matches.Count);
+
+            foreach(Match hit in matches)
+            {
+                GroupCollection groupCollection = hit.Groups;
+                Console.WriteLine("Found {0} at index {1}",groupCollection[0].Value,groupCollection[0].Index);
+            }
+        }
+    }
+}
